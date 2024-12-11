1ea2d0b86926f5ab03acae5c84beb19b33a8ee11
Gabriel Milioli <gabrielpaidosteve@gmail.com>
2024-12-10 23:16:29 -0300

    Commit inicial

---
+++ src/main/java/com/milioli/musikos/MusikosApplication.java
@@ -0,0 +1,13 @@
+package com.milioli.musikos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusikosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusikosApplication.class, args);
+	}
+
+}
