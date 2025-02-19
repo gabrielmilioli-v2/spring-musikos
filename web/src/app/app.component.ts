ce0e70836bfbdacb14bd32d9543b23bc7e2f3f0f
Gabriel Milioli <gabrielpaidosteve@gmail.com>
2025-02-19 18:10:11 -0300

    Material and tailwindcss

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatButtonModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'musikos';
